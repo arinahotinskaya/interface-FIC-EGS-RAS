ba2f780832162654153f47e09ff762bd7c1d71f9
arinahotinskaya <arinakhotinskaya@mail.ru>
2025-07-07 12:19:02 +0300

    Update card page, correct syntax warning in Registration.jsx

--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -1,4 +1,9 @@
 import "./Cards.scss"
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import russianBorder from '../../assets/russian.json';
+import {position, stations} from '../../assets/constants';
+
 
 function Cards() {
   return (
@@ -6,6 +11,38 @@ function Cards() {
         <section className="cards">
             <div className="cards__container">
                 <h2 className="cards__title">Карта сети станций ГС РАН</h2>
+                <div className="cards__map-container">
+                  <div className="cards__map">
+                    <MapContainer center={position} zoom={3} style={{height: '100%', width: '100%'}} attributionControl={false}>
+                      <TileLayer
+                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                        attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+                      />
+                      <GeoJSON data={russianBorder.features} className="cards__map--border" style={{ color: 'gray', weight: 1.25, fill: true }}/>
+                      {stations.map(station => {
+                        return (
+                          <Marker key={station.name} position={station.coords}>
+                            <Popup>
+                              <h3>Станция: {station.name}</h3>
+                              <p>Координаты: {station.coords.join(', ')}</p>
+                            </Popup>
+                          </Marker>
+                        )
+                      })}
+                    </MapContainer>
+                  </div>
+                  <div className="cards__map-info">
+                    <ul className="cards__map-info-list">
+                      {stations.map(station => {
+                        return (
+                          <li className="cards__map-info-item" key={station.name}>
+                            <span className="cards__map-info-item-title">{station.name}</span>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
             </div>
         </section>
     </>
