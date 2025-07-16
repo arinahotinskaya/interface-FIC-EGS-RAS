b7ab39d8f4c34ba1fdb8a07239cb14c3ca22bd68
arinahotinskaya <arinakhotinskaya@mail.ru>
2025-07-16 12:20:52 +0300

    Make a page for information

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Header from "./pages/Header/Header"
 import {Route, Routes} from 'react-router-dom'
-import Cards from "./components/Cards/Cards"
-import Stations from "./components/ArchiveAccess/ArchiveAccess"
+import Header from "./pages/Header/Header"
+import Footer from "./pages/Footer/Footer"
 import Registration from "./pages/Authorization/Registration/Registration"
 import Login from "./pages/Authorization/Login/Login"
 import "./pages/Authorization/Authorization.scss"
 import StationsPage from "./pages/StationsPage/StationsPage"
-import Footer from "./pages/Footer/Footer"
 import AccessPage from "./pages/AccessPage/AccessPage"
+import InfoPage from "./pages/InfoPage/InfoPage"
 
 
 function App() {
@@ -17,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/stations-page" element={<StationsPage />} />
         <Route path="/access-page" element={<AccessPage />} />
+        <Route path='/info-page' element={<InfoPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
